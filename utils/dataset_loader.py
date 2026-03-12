189ca93d2d9a96d5a6797604cccf506de3b13233
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-03-12 17:11:24 +0530

    Update dataset_loader.py

--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
 
 modulation_map = {
 "BPSK":0,
@@ -19,29 +20,47 @@ modulation_map = {
 
 class RFdataset(Dataset):
 
-    def __init__(self,root,task):
+    def __init__(self, root, task, split="train"):
 
         self.samples = []
         self.task = task
 
         if task == "jam":
-
             self.load_jam_dataset(root)
 
         elif task == "mix":
-
             self.load_mix_dataset(root)
 
         elif task == "mod":
-
             self.load_mod_dataset(root)
 
         elif task == "dual":
-
             self.load_dual_dataset(root)
 
+        # -------- SPLIT DATASET --------
+        train, temp = train_test_split(
+            self.samples,
+            test_size=0.30,
+            random_state=42
+        )
 
-    def load_jam_dataset(self,root):
+        val, test = train_test_split(
+            temp,
+            test_size=0.50,
+            random_state=42
+        )
+
+        if split == "train":
+            self.samples = train
+
+        elif split == "val":
+            self.samples = val
+
+        elif split == "test":
+            self.samples = test
+
+
+    def load_jam_dataset(self, root):
 
         clean_path = os.path.join(root,"clean")
         jam_path = os.path.join(root,"jammed")
@@ -52,7 +71,9 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),0))
+                self.samples.append(
+                    (os.path.join(folder,file),0)
+                )
 
 
         for mod in os.listdir(jam_path):
@@ -61,10 +82,12 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),1))
+                self.samples.append(
+                    (os.path.join(folder,file),1)
+                )
 
 
-    def load_mix_dataset(self,root):
+    def load_mix_dataset(self, root):
 
         clean_path = os.path.join(root,"clean")
         mixed_path = os.path.join(root,"mixed")
@@ -75,7 +98,9 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),0))
+                self.samples.append(
+                    (os.path.join(folder,file),0)
+                )
 
 
         for pair in os.listdir(mixed_path):
@@ -84,10 +109,12 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),1))
+                self.samples.append(
+                    (os.path.join(folder,file),1)
+                )
 
 
-    def load_mod_dataset(self,root):
+    def load_mod_dataset(self, root):
 
         clean_path = os.path.join(root,"clean")
 
@@ -99,10 +126,12 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),label))
+                self.samples.append(
+                    (os.path.join(folder,file),label)
+                )
 
 
-    def load_dual_dataset(self,root):
+    def load_dual_dataset(self, root):
 
         mixed_path = os.path.join(root,"mixed")
 
@@ -119,7 +148,9 @@ class RFdataset(Dataset):
 
             for file in os.listdir(folder):
 
-                self.samples.append((os.path.join(folder,file),label))
+                self.samples.append(
+                    (os.path.join(folder,file),label)
+                )
 
 
     def __len__(self):
@@ -127,7 +158,7 @@ class RFdataset(Dataset):
         return len(self.samples)
 
 
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
 
         path,label = self.samples[idx]
 
