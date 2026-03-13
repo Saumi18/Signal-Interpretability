5ba01718414d27fc68ed8dbc7bfc14ae2610c97d
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-03-13 08:48:37 +0530

    Update train_dual_modulation.py

--- Training/train_dual_modulation.py
+++ Training/train_dual_modulation.py
@@ -2,36 +2,75 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from utils.dataset_loader import RFdataset
-from models.full_model import RFSignalModel
-
+from models.classifiers import RFClassifier
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataset = RFdataset("dataset","dual")
+BATCH_SIZE = 32
+EPOCHS = 25
+LR = 1e-3
 
-loader = DataLoader(dataset,batch_size=32,shuffle=True)
+dataset_path = "/content/drive/MyDrive/spectrogram_matrices_dataset"
 
-model = RFSignalModel().to(device)
+train_dataset = RFdataset(dataset_path,"dual","train")
+val_dataset = RFdataset(dataset_path,"dual","val")
 
-criterion = nn.BCELoss()
+train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True)
+val_loader = DataLoader(val_dataset,batch_size=BATCH_SIZE)
 
-optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
+model = RFClassifier().to(device)
 
-for epoch in range(15):
+criterion = nn.BCEWithLogitsLoss()
 
-    for x,y in loader:
+optimizer = torch.optim.Adam(model.parameters(),lr=LR)
+
+for epoch in range(EPOCHS):
+
+    model.train()
+
+    total_loss = 0
+
+    for x,y in tqdm(train_loader):
 
         x = x.to(device)
         y = y.to(device).float()
 
-        _,_,_,dual_out = model(x)
-
-        loss = criterion(dual_out,y)
-
         optimizer.zero_grad()
+
+        features = model.backbone(x)
+
+        out = model.dual_classifier(features)
+
+        loss = criterion(out,y)
 
         loss.backward()
 
         optimizer.step()
 
-    print("Epoch",epoch,"done")
+        total_loss += loss.item()
+
+    print("Epoch",epoch,"Loss",total_loss/len(train_loader))
+
+    model.eval()
+
+    with torch.no_grad():
+
+        total_loss = 0
+
+        for x,y in val_loader:
+
+            x = x.to(device)
+            y = y.to(device).float()
+
+            features = model.backbone(x)
+
+            out = model.dual_classifier(features)
+
+            loss = criterion(out,y)
+
+            total_loss += loss.item()
+
+    print("Validation Loss:",total_loss/len(val_loader))
+
+torch.save(model.state_dict(),"dual_modulation_model.pth")
