9e51448776572da056ca139284d352188537bbb4
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-03-13 08:51:40 +0530

    Update full_model.py

--- Models/full_model.py
+++ Models/full_model.py
@@ -1,11 +1,8 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from models.backbone import RFBackbone
-from models.classifiers import JammingClassifier, MixedClassifier, ModulationClassifier, DualModulationClassifier
 import torch.nn as nn
+from .backbone import RFBackbone
+from .classifiers import *
 
-class RFSignalModel(nn.Module):
+class RFClassifier(nn.Module):
 
     def __init__(self):
 
@@ -20,17 +17,3 @@ class RFSignalModel(nn.Module):
         self.mod_classifier = ModulationClassifier()
 
         self.dual_classifier = DualModulationClassifier()
-
-    def forward(self,x):
-
-        features = self.backbone(x)
-
-        jam_out = self.jam_classifier(features)
-
-        mix_out = self.mix_classifier(features)
-
-        mod_out = self.mod_classifier(features)
-
-        dual_out = self.dual_classifier(features)
-
-        return jam_out,mix_out,mod_out,dual_out
