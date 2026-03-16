1f835ec0a97f9326d698f00008d53aece46cb5fa
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-03-16 11:29:29 +0530

    added forward method

--- models/full_model.py
+++ models/full_model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
-from .backbone import RFBackbone
-from .classifiers import *
+from models.backbone import RFBackbone
+from models.classifiers import JammingClassifier, MixedClassifier, ModulationClassifier, DualModulationClassifier
 
 class RFClassifier(nn.Module):
 
@@ -17,3 +17,10 @@ class RFClassifier(nn.Module):
         self.mod_classifier = ModulationClassifier()
 
         self.dual_classifier = DualModulationClassifier()
+
+    def forward(self, x):
+        features = self.backbone(x)
+        jam_pred = self.jam_classifier(features)
+        mix_pred = self.mix_classifier(features)
+        mod_pred = self.mod_classifier(features)
+        return jam_pred, mix_pred, mod_pred
